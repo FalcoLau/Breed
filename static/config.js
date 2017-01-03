4daf729ff4525a544d0837c40c911e39a6fe3596
FalcoLau <502773516@qq.com>
2017-01-03 10:28:31 +0800

    Update config.js

--- static/config.js
+++ static/config.js
@@ -2,7 +2,7 @@
 // ID of the Google spreadsheet
 // ***********************************************************************************
 // Make sure your spreadsheet is published (File > Publish to the web…)
-var spreadsheetId = "1P9wMb9e0YbhcOua9RQeRCJjllf_L77uV-7i4Q0Yor0o";
+var spreadsheetId = "1jsITIEvHzYUOJc0T0ZssqAnZDi4eTpI8UXtoC74VCvc";
 
 // ***********************************************************************************
 // Position of Breedables, Shinies and Wants worksheet
@@ -15,13 +15,13 @@ var wantsWorksheet = 3;
 // ***********************************************************************************
 // Your FC and IGN
 // ***********************************************************************************
-var friendCode = "4597-0550-9417";
-var inGameName = "Richie";
+var friendCode = "3368-4685-7606";
+var inGameName = "Falco";
 
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-var contactUrl = "http://reddit.com/u/richi3f";
+var contactUrl = "http://reddit.com/u/Falco_Lau";
 
 // ***********************************************************************************
 // URL of your PGL trainer icon (optional)
